351874a5f181b0d584ac87a5a6506856ce92f68d
Slava Dergachyov <mr.hadros@gmail.com>
2023-01-11 19:35:33 +0300

    ADDED: phpstan.

--- src/Controller/User/RegistrationController.php
+++ src/Controller/User/RegistrationController.php
@@ -7,7 +7,7 @@ use App\Form\User\RegistrationFormType;
 use App\Security\EmailVerifier;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mime\Address;
